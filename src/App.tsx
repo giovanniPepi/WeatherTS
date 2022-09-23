9b6012313a1ec156b408ce3eb6428ca77ceba74a
giovanniPepi <gtpepi@hotmail.com>
2022-09-23 16:05:51 -0300

    Add initial structure and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import getWeatherAPI from './functions/getWeatherAPI';
 
-function App() {
+
+
+const App = () => {
+    console.log(getWeatherAPI(-22.90556, -47.06083, 'Campinas, BR'))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <div>hello</div>
+
+  )
 }
 
 export default App;
---
+++ src/functions/getWeatherAPI.ts
@@ -0,0 +1,23 @@
+const getWeatherAPI = async (lat: number, lon: number, country:string) =>  {
+    if (country === 'BR') {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=833c261d19926d13cc578d79528d9d64&units=metric&lang=pt_br`,
+      {
+        mode: 'cors'
+      }
+    );
+    const receivedData = await response.json();
+    return receivedData;
+    }
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=833c261d19926d13cc578d79528d9d64&units=metric`,
+    {
+      mode: 'cors'
+    }
+  );
+  // Returns a promise that resolves with the result of parsing the response body text as JSON
+  const receivedData = await response.json();
+  return receivedData;
+}
+
+export default getWeatherAPI;
