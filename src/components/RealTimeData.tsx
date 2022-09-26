5f5675144534193a5057f583c246db8aaba1c048
giovanniPepi <gtpepi@hotmail.com>
2022-09-26 17:38:28 -0300

    Add km converter

--- src/components/RealTimeData.tsx
+++ src/components/RealTimeData.tsx
@@ -1,4 +1,5 @@
 import { RealTimeDataProps } from "interfaces";
+import convertToKm from "src/functions/convertToKm";
 import getExactHours from "src/functions/getExactHour";
 
 // dealing with objects as props, they must have their own interface:
@@ -14,18 +15,19 @@ const RealTimeData: React.FC<RealTimeDataProps> = ({
     <section className="realTimeData">
       <div>Updated at: {getExactHours(apiData.current.dt)}</div>
       <div>{locationToShow}</div>
+      <div>{apiData.current.weather[0].description}</div>
+      <div>Temp: {apiData.current.temp}</div>
+      <div>Feels_like: {apiData.current.feels_like} ºC</div>
+      <div>UV: {apiData.current.uvi}</div>
       <div>Clouds: {apiData.current.clouds} %</div>
-      <div>Dew Point: {apiData.current.dew_point}</div>
-      <div>Feels_like: {apiData.current.feels_like}</div>
-      <div>Humidity: {apiData.current.humidity}</div>
-      <div>Pressure: {apiData.current.pressure}</div>
+      <div>Dew Point: {apiData.current.dew_point} ºC</div>
+      <div>Humidity: {apiData.current.humidity} %</div>
+      <div>Pressure: {apiData.current.pressure} hPa</div>
       <div>Sunrise: {getExactHours(apiData.current.sunrise)}</div>
       <div>Sunset: {getExactHours(apiData.current.sunset)}</div>
-      <div>Temp: {apiData.current.temp}</div>
-      <div>UVI: {apiData.current.uvi}</div>
-      <div>Visibility: {apiData.current.visibility}</div>
-      <div>Weather desc: {apiData.current.weather[0].description}</div>
-      <div>Weather main desc: {apiData.current.weather[0].main}</div>
+      <div>Visibility: {apiData.current.visibility / 1000} km</div>
+      <div>Wind direction: {apiData.current.wind_deg}</div>
+      <div>Wind speed: {convertToKm(apiData.current.wind_speed)} km/h</div>
     </section>
   );
 };
