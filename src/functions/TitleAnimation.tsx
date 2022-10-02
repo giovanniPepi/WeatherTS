1a02d2b33f88e1689cd160335b7fbb9978c92fd7
giovanniPepi <gtpepi@proton.me>
2022-10-02 11:33:06 -0300

    Update animation speed

--- src/functions/TitleAnimation.tsx
+++ src/functions/TitleAnimation.tsx
@@ -10,108 +10,118 @@ const animation = keyframes`
 100% {opacity: 0; transform: translateX(-200px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px);}`;
 
 const Wrapper = styled.div`
-  display: inline-block;
+  display: inline;
   span {
-    display: inline-block;
+    display: inline;
     opacity: 0;
     animation-name: ${animation};
     animation-duration: 15s;
     animation-fill-mode: forwards;
     animation-iteration-count: infinite;
+    animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  span > * {
+    display: inline;
   }
   span:nth-child(1) {
-    animation-delay: 0.1s;
+    animation-delay: 0.05s;
   }
   span:nth-child(2) {
-    animation-delay: 0.2s;
+    animation-delay: 0.1s;
   }
   span:nth-child(3) {
-    animation-delay: 0.3s;
+    animation-delay: 0.15s;
   }
   span:nth-child(4) {
-    animation-delay: 0.4s;
+    animation-delay: 0.2s;
   }
   span:nth-child(5) {
-    animation-delay: 0.5s;
+    animation-delay: 0.25s;
   }
   span:nth-child(6) {
-    animation-delay: 0.6s;
+    animation-delay: 0.3s;
   }
 
   span:nth-child(7) {
-    animation-delay: 0.7s;
+    animation-delay: 0.35s;
   }
 
   span:nth-child(8) {
-    animation-delay: 0.8s;
+    animation-delay: 0.4s;
   }
 
   span:nth-child(9) {
-    animation-delay: 0.9s;
+    animation-delay: 0.45s;
   }
 
   span:nth-child(10) {
-    animation-delay: 1s;
+    animation-delay: 0.5s;
   }
 
   span:nth-child(11) {
-    animation-delay: 1.1s;
+    animation-delay: 0.55s;
   }
 
   span:nth-child(12) {
-    animation-delay: 1.2s;
+    animation-delay: 0.6s;
   }
 
   span:nth-child(13) {
-    animation-delay: 1.3s;
+    animation-delay: 0.65s;
   }
 
   span:nth-child(14) {
-    animation-delay: 1.4s;
+    animation-delay: 0.7s;
   }
 
   span:nth-child(15) {
-    animation-delay: 1.5s;
+    animation-delay: 0.75s;
   }
 
   span:nth-child(16) {
-    animation-delay: 1.6s;
+    animation-delay: 0.8s;
   }
 
   span:nth-child(17) {
-    animation-delay: 1.7s;
+    animation-delay: 0.85s;
   }
 
   span:nth-child(18) {
-    animation-delay: 1.8s;
+    animation-delay: 0.9s;
   }
 
   span:nth-child(19) {
-    animation-delay: 1.9s;
+    animation-delay: 0.95s;
   }
 
   span:nth-child(20) {
-    animation-delay: 2;
+    animation-delay: 1s;
   }
 
   span:nth-child(21) {
-    animation-delay: 2.1s;
+    animation-delay: 1.05s;
   }
 
   span:nth-child(22) {
-    animation-delay: 2.2s;
+    animation-delay: 1.1s;
   }
 
   span:nth-child(23) {
-    animation-delay: 2.3s;
+    animation-delay: 1.15s;
   }
 
   span:nth-child(24) {
-    animation-delay: 2.4s;
+    animation-delay: 1.2s;
   }
 
   span:nth-child(25) {
-    animation-delay: 2.5s;
+    animation-delay: 1.25s;
+  }
+  span:nth-child(26) {
+    animation-delay: 1.3s;
+  }
+  span:nth-child(27) {
+    animation-delay: 1.35s;
   }
 `;
 
