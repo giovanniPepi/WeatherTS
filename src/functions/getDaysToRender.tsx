4fd8a55bff54b2473e2f0c95dfe3a4702413c9e0
giovanniPepi <gtpepi@proton.me>
2022-10-07 10:31:41 -0300

    Update cloudBg, update daily modal style

--- src/functions/getDaysToRender.tsx
+++ src/functions/getDaysToRender.tsx
@@ -3,7 +3,7 @@ const getDaysToRender = (): number => {
   let daysToRender = 0;
 
   if (screenWidth < 950) daysToRender = 1;
-  else if (screenWidth >= 950) daysToRender = 3;
+  else if (screenWidth >= 950) daysToRender = 2;
 
   return daysToRender;
 };
